a1c626c3e12773810016e111287f83128c4ebf76
Samira Ouaaz <souaaz@yahoo.com>
2022-02-12 21:12:38 -0500

    Week 5 - Adding Alert Dialog to ListView app

--- app/src/main/java/com/cst3104/samples/FirstActivity.java
+++ app/src/main/java/com/cst3104/samples/FirstActivity.java
@@ -1,7 +1,6 @@
-package com.cst2335.exercises;
+package com.cst3104.samples;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
